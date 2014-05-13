2d89cdf1a8552a12385b43f3b3adb86d83523fdb
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-05-13 18:43:55 -0400

    Images all load when page is ready

--- day18/animations.js
+++ day18/animations.js
@@ -7,12 +7,13 @@ var images = [{path: "car.png", caption: "Ceci n'est pas une voiture"},
 	{path:"pipe.png", caption: "Ceci n'est pas une pipe"},
 	{path:"computer.png", caption: "Ceci n'est pas un ordinateur"},
 	{path:"tv.png", caption: "Ceci n'est pas un televiseur"},
-	{path:"shirt.png", caption: "Ceci n'est pas une chemise"},
-	{path: "chair.png", caption: "Ceci n'est pas une chaise"}];
+	{path:"shirt.png", caption: "Ceci n'est pas une chemise"}];
+var loadedImages = [];
 var $currentImage;
 var $currentCaption;
 
 $(document).ready(function() {
+	init();
 	// show instruction, then show first image
 	$("h1").delay(500).fadeOut(1000, function() {
 		newArt();
@@ -34,12 +35,21 @@ $(document).ready(function() {
 });
 
 /*
+	Load all images at first.
+*/
+function init() {
+	for (var i = 0; i < images.length; i++) {
+		loadedImages.push({image:$("<img src='./images/"+images[i].path+"'></img>"), caption: $("<h1>"+images[i].caption+".</h1>")});
+	};
+}
+
+/*
 	Takes care of adding a new image and caption to the page.
 */
 function newArt() {
 	var i = Math.floor(Math.random()*images.length);
-	$currentImage = $("<img src='./images/"+images[i].path+"'></img>");
-	$currentCaption = $("<h1>"+images[i].caption+".</h1>");
+	$currentImage = loadedImages[i].image;
+	$currentCaption = loadedImages[i].caption;
 	$(".canvas").append($currentImage);
 	$currentImage.delay(100).fadeIn("fast");
 	$(".canvas").append($currentCaption);
