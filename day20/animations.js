a2d9a8b5c9ec20c22c9d4ef27de3a5460788386c
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-05-16 11:34:57 -0400

    Finished day 20

--- day20/animations.js
+++ day20/animations.js
@@ -2,15 +2,25 @@
 *	Designed and programmed by Mohamed Adam Chaieb.
 *****************************************************/
 
+var enabled = true;
+
 $(document).ready(function() {
+	$(".instruction").delay(2000).fadeOut();
 	$(".all").mousemove(function(event) {
-		$(".all").css("background-color", randomColor(event.pageY, event.pageX));
+		if(enabled)
+			$(".all").css("background-color", randomColor(event.pageY, event.pageX));
+	});
+	$(".all").click(function() {
+		enabled = !enabled
 	});
 });
 
 function randomColor(r,g) {
-	var red = Math.floor(r/$(window).height())*255;
-	var green = Math.floor(g/$(window).width())*255;
-	var b = 60;
-	return "rgb("+red+","+green+","+b+")";
+	var red = Math.floor((r-50)/(0.91*$(window).height())*255);
+	var green = Math.floor(g/$(window).width()*256);
+	var blue = Math.floor((red+green)/2);
+	$(".rgb").html("<p class='rgb'>R:"+red+" G:"+green+" B:"+blue+"</p>");
+	if((blue+red+green)/3 > 180)
+		$(".rgb").css("color", "#222");
+	return "rgb("+red+","+green+","+blue+")";
 };
