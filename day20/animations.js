42a7d398d156ffccbf87ab5146f2261798093261
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-05-17 11:31:01 -0400

    Fixed day 20, added day 22

--- day20/animations.js
+++ day20/animations.js
@@ -5,22 +5,25 @@
 var enabled = true;
 
 $(document).ready(function() {
-	$(".instruction").delay(2000).fadeOut();
+	$(".instruction").delay(5000).fadeOut();
 	$(".all").mousemove(function(event) {
 		if(enabled)
-			$(".all").css("background-color", randomColor(event.pageY, event.pageX));
+			$(".all").css("background-color", newColor(event.pageY, event.pageX));
 	});
 	$(".all").click(function() {
 		enabled = !enabled
 	});
 });
 
-function randomColor(r,g) {
-	var red = Math.floor((r-50)/(0.91*$(window).height())*255);
-	var green = Math.floor(g/$(window).width()*256);
-	var blue = Math.floor((red+green)/2);
+function newColor(x,y) {
+	var w = $(window).width();
+	var h = $(window).height();
+	var midY = h/2;
+	var red = Math.floor((x-50)/(0.91*h)*255);
+	var green = Math.floor(y/w*256);
+	var blue = Math.floor(255*(Math.abs(2*y-h)/h));
 	$(".rgb").html("<p class='rgb'>R:"+red+" G:"+green+" B:"+blue+"</p>");
-	if((blue+red+green)/3 > 180)
+	if((blue+red+green)/3 > 150)
 		$(".rgb").css("color", "#222");
 	return "rgb("+red+","+green+","+blue+")";
 };
