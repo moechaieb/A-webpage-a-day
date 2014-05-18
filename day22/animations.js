c6e352b31258a6e910e391182461a0d5148d1c1f
Mohamed Adam Chaieb <mohamed.chaieb@mail.mcgill.ca>
2014-05-18 11:06:47 -0400

    Re-pushing day 22 changes

--- day22/animations.js
+++ day22/animations.js
@@ -2,6 +2,29 @@
 *	Designed and programmed by Mohamed Adam Chaieb.
 *****************************************************/
 
+var pictures = ["banana.jpg", "che.jpg", "gum.jpg", "jackson.jpg", "kfc.jpg", "lennon.png", "playstation.jpg", "soup.jpg"];
+var i = Math.floor(Math.random()*pictures.length);
+
 $(document).ready(function() {
-	
+	$("header").animate({
+		"font-size": "10em"
+	}, 400);
+	$("header").delay(1000).animate({
+		"top": "0",
+		"margin-top": "0"
+	}, function() {
+		init();
+	});
 });
+
+function init() {
+	$(".slideshow").html("<img src='./images/"+pictures[i]+"'></img>");
+	$(".slideshow").fadeIn();
+	setInterval(function() {
+		i++;
+		$(".slideshow").fadeOut(1000, function() {
+			$(".slideshow").html("<img src='./images/"+pictures[i%pictures.length]+"'></img>");
+			$(".slideshow").fadeIn();
+		});
+	}, 3000);
+};
